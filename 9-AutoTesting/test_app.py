f8b7e170e85c9d212b3661b139120824a3b07c1e
JoaoSantosPereira <joaopereira18@gmail.com>
2025-08-07 11:42:34 +0100

    Docker Container added

--- 9-AutoTesting/test_app.py
+++ 9-AutoTesting/test_app.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest # type: ignore
 from app import app
 
 @pytest.fixture
@@ -13,12 +13,36 @@ def test_ping(client):
 
 def test_post_task_valid(client):
     payload = {
-        "title": "Estudar para a entrevista",
+        "title": "Learning Python APIs",
         "done": False
     }
     response = client.post("/tasks", json=payload)
     assert response.status_code == 201
     data = response.get_json()
-    assert data["title"] == "Estudar para a entrevista"
+    assert data["title"] == payload["title"]
     assert data["done"] is False
     assert "id" in data
+
+def test_post_task_missing_title(client):
+    payload = {"done": True} # missing "title" key
+    response = client.post("/tasks", json=payload)
+    assert response.status_code == 400
+    assert "title" in response.get_json()["error"].lower()
+
+def test_post_task_short_title(client):
+    payload = {"title": "aa", "done": True} # title too short
+    response = client.post("/tasks", json=payload)
+    assert response.status_code == 400
+    assert "title" in response.get_json()["error"].lower()
+
+def test_post_task_missing_done(client):
+    payload = {"title": "Study Flask"} # missing "done" key
+    response = client.post("/tasks", json=payload)
+    assert response.status_code == 400
+    assert "done" in response.get_json()["error"].lower()
+
+def test_post_task_invalid_done_type(client):
+    payload = {"title": "Learning Docker", "done": "yes"} # done wrong type
+    response = client.post("/tasks", json=payload)
+    assert response.status_code == 400
+    assert "done" in response.get_json()["error"].lower()
